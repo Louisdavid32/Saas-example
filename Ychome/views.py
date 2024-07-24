76ddceb4844dd6ee49b8f2cf88b1ffda3de3d39f
Louisdavid32 <omgbal78@gmail.com>
2024-07-24 13:56:40 +0200

    ycoder

---
+++ Ychome/views.py
@@ -0,0 +1,43 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+
+this_dir =  pathlib.Path(__file__).resolve().parent
+
+
+
+def home_page_view(request, *args, **kwargs):
+    
+    qs = PageVisit.objects.all()
+    
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_page = 'My page'
+    my_context = {
+        'page_title':my_page,
+        'page_visit_count':page_qs.count(),
+        'tota_visite_count': qs.count(),
+        'percent': page_qs.count() * 100/ qs.count()        
+        }
+    PageVisit.objects.create(path=request.path)
+    return render(request, 'home.html', my_context)
+
+def home_old_page_view(request):
+    print(this_dir)
+    html_="""    <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>hello is youncoder </h1>
+            </body>
+            </html>"""
+            
+    html_file_pht = this_dir/'home.html'
+    html_ = html_file_pht.read_text()
+    return HttpResponse(html_)
